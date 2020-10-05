dc65e4a836683539acbadff2786ce03eff028e64
unknown <Soumak Maji>
2020-10-06 00:18:18 +0530

    basic authentication added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const dishRouter = require('./Routes/dishRouter');
 const promotionsRouter=require('./Routes/PromoRouter')
 const leadersRouter=require('./Routes/leaderRouter')
 const mongoose = require('mongoose');
+var path = require('path');
+var cookieParser = require('cookie-parser');
 
 const Dishes = require('./models/dishes');
 const hostname = 'localhost';
@@ -25,18 +27,56 @@ mongoose.connect(url,{
 
 
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(bodyParser.json())
+function auth (req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+}
+
+app.use(auth);
 app.use(dishRouter);
 app.use(promotionsRouter);
 app.use(leadersRouter);
-app.use(morgan('dev'));
-app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 const server = http.createServer(app);
-app.use(function(err,req,res,next){
-  res.locals.message=err.message;
-  res.locals.error='development';
-  res.status(err.status||500);
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
   res.render('error');
-})
+});
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
